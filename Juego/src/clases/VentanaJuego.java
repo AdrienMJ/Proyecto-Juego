8d22b287c5e21a8f36596463ce2b993dd8e07956
Adrien Martinez <adrien.m.j@opendeusto.es>
2024-10-06 12:06:28 +0200

    Intento de cracion de paneles para el juego

--- Juego/src/clases/VentanaJuego.java
+++ Juego/src/clases/VentanaJuego.java
@@ -1,5 +1,9 @@
 package clases;
 
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -15,6 +19,29 @@ public class VentanaJuego extends JFrame {
         JPanel objetosPanel = new JPanel();
         JPanel mejorasPanel = new JPanel();
         
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
 
+        clickerPanel.setBackground(Color.blue);
+
+        objetosPanel.setBackground(Color.GREEN);
+
+        mejorasPanel.setBackground(Color.ORANGE);
+
+        gbc.gridx = 0; 
+        gbc.gridy = 0; 
+        panel.add(clickerPanel, gbc);
+
+        gbc.gridx = 1; 
+        gbc.gridy = 0; 
+        panel.add(objetosPanel, gbc);
+
+        // Agregar el tercer panel en (0, 1)
+        gbc.gridx = 0; // Columna 0
+        gbc.gridy = 1; // Fila 1
+        gbc.gridwidth = 2; // Este panel ocupa ambas columnas
+        panel.add(mejorasPanel, gbc);
+        
+        add(panel);
 	}
 }
