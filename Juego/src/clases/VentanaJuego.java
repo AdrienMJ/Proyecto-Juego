8e5aa242c14e5639826e893d3da1a1287f679562
Adrien Martinez <adrien.m.j@opendeusto.es>
2024-10-07 10:35:48 +0200

    Mas intentos para formar las ventanas

--- Juego/src/clases/VentanaJuego.java
+++ Juego/src/clases/VentanaJuego.java
@@ -1,5 +1,6 @@
 package clases;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -10,38 +11,30 @@ import javax.swing.JPanel;
 
 public class VentanaJuego extends JFrame {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public VentanaJuego() {
+		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setTitle("EstudianteClicker");
-        setSize(1600,900);
+        setSize(600,400);
+        
+        setLayout(new BorderLayout());
         
         JPanel clickerPanel = new JPanel();
         JPanel objetosPanel = new JPanel();
         JPanel mejorasPanel = new JPanel();
-        
-        JPanel panel = new JPanel(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-
         clickerPanel.setBackground(Color.blue);
-
         objetosPanel.setBackground(Color.GREEN);
-
         mejorasPanel.setBackground(Color.ORANGE);
-
-        gbc.gridx = 0; 
-        gbc.gridy = 0; 
-        panel.add(clickerPanel, gbc);
-
-        gbc.gridx = 1; 
-        gbc.gridy = 0; 
-        panel.add(objetosPanel, gbc);
-
-        // Agregar el tercer panel en (0, 1)
-        gbc.gridx = 0; // Columna 0
-        gbc.gridy = 1; // Fila 1
-        gbc.gridwidth = 2; // Este panel ocupa ambas columnas
-        panel.add(mejorasPanel, gbc);
+        add(clickerPanel, BorderLayout.WEST);
+        add(objetosPanel, BorderLayout.SOUTH);
+        add(mejorasPanel, BorderLayout.EAST);
         
-        add(panel);
+
+        
 	}
 }
