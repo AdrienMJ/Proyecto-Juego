cdb1118fae76c555bcc42a01c8e8db0429f4aac2
Adrien Martinez <adrien.m.j@opendeusto.es>
2024-10-26 14:10:03 +0200

    Clase Mejora -> Se crea un objeto(en este caso un material de estudio o similar). Faltaría añadir tema de metodos para que se actualice el nº de objetos(edificios en coockiecliker) al comprar, que aumente el precio y lo que genera

--- Juego/src/clases/Mejora.java
+++ Juego/src/clases/Mejora.java
@@ -5,4 +5,53 @@ public class Mejora {
 	private float precio;
 	private float ganacias;
 	private int numero;
+	
+	/**Crea un objeto Mejora, que es el propio material como: lapiz,libro,cuaderno....
+	 * Se puede saber y cambiar su nombre, precio y ganancia(cuanto multiplica)
+	 * 
+	 * @param nombre Nombre del material escolar
+	 * @param precio Precio del material escolar
+	 * @param ganacias Cuanto multiplica a las ganacias totales
+	 */
+	public Mejora(String nombre, float precio, float ganacias) {
+		super();
+		this.nombre = nombre;
+		this.precio = precio;
+		this.ganacias = ganacias;
+		this.numero = 0;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+
+	public float getGanacias() {
+		return ganacias;
+	}
+
+	public void setGanacias(float ganacias) {
+		this.ganacias = ganacias;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	
 }
