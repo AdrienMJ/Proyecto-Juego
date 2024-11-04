c0f74b801b725e74ab5a92d1cedc7b54b9c238c9
Jon Xabier Ezpeleta <j.ezpeleta@opendeusto.es>
2024-11-04 16:02:50 +0100

    Merge branch 'master' of https://github.com/AdrienMJ/Proyecto-Juego

--- Juego/src/main/Main.java
+++ Juego/src/main/Main.java
@@ -3,7 +3,6 @@ package main;
 import javax.swing.SwingUtilities;
 
 import clases.VentanaJuego;
-import tema3.tema3A.EjemploJButton;
 
 public class Main {
 	public static void main(String[] args) {
