d40307ec3458fae82b1bf9e90f58683b6f756493
Adrien Martinez <adrien.m.j@opendeusto.es>
2024-11-25 11:04:02 +0100

    Creada la parte visual de la VentanaLogin

--- Juego/src/clases/VentanaLogin.java
+++ Juego/src/clases/VentanaLogin.java
@@ -1,7 +1,13 @@
 package clases;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
@@ -14,16 +20,45 @@ public class VentanaLogin extends JFrame {
 	
 	private JLabel labelUsuario;
 	private JTextField usuario;
-	private JLabel labelcontraseña;
+	private JLabel labelContraseña;
 	private JPasswordField contraseña;
+	private JButton inicioSesion;
+	private JButton registrarse;
 	
 	public VentanaLogin() {
 		
-		setSize(300,200);
+		setSize(400,300);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Login");
-		setVisible(true);
 		setLocationRelativeTo(null);
+		
+		//Componentes
+		JLabel labelBienvenida = new JLabel("  Hola! Introduce tu usuario y contraseña porfavor! \n O registrate si aún no lo has hecho!");
+		labelUsuario = new JLabel("  Usuario");
+		labelContraseña = new JLabel("  Contraseña");
+		usuario = new JTextField();
+		contraseña = new JPasswordField();
+		inicioSesion = new JButton("Iniciar Sesion");
+		registrarse = new JButton("Registrarse");
+			
+		JPanel panelCampos = new JPanel();
+		panelCampos.setLayout(new GridLayout(3,2,3,10));
+		panelCampos.add(labelUsuario);
+		panelCampos.add(usuario);
+		panelCampos.add(labelContraseña);
+		panelCampos.add(contraseña);
+		panelCampos.add(inicioSesion);
+		panelCampos.add(registrarse);
+		
+		setVisible(true);
+		
+		this.add(labelBienvenida, BorderLayout.NORTH);
+		this.add(panelCampos, BorderLayout.CENTER);
+		
+		
+		
+		
+		
 		
 		
 	}
