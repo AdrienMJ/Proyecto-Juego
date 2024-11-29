f5ba8d648b3d6ec4a155a44f229c4570b306b0ed
Asier Miguel <a.miguel@opendeusto.es>
2024-11-29 10:11:02 +0100

    Se ha aplicado un formato a la produccion por si hay demasiados decimales

--- Juego/src/clases/RendererTablaEstadis.java
+++ Juego/src/clases/RendererTablaEstadis.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.text.DecimalFormat;
 
 import javax.swing.JLabel;
 import javax.swing.JTable;
@@ -38,6 +39,23 @@ public class RendererTablaEstadis extends DefaultTableCellRenderer {
 			miCelda.setBackground(new Color(255,196,242));
 		}
 		
+		if (column == 2) {
+			
+			try {
+			    //Convertir el texto a un número float:
+			    float numeroDecimal = Float.parseFloat(miCelda.getText());
+
+			    //Formato:
+			    DecimalFormat formato = new DecimalFormat("#.#");
+			    String numeroFormateado = formato.format(numeroDecimal);
+
+			    miCelda.setText(numeroFormateado);
+			
+			} catch (NumberFormatException e) {
+				
+			    System.err.println("Error: el texto no es un número válido.");
+			    miCelda.setText("0.00");
+			}		}
 		
 		
 		return miCelda;
