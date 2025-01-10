0c7afedb8d509a2e16f4ea508658b021cd6f35bd
Adrien Martinez <adrien.m.j@opendeusto.es>
2025-01-10 16:55:49 +0100

    metodos cargarUsuario y comprobarSiUsuarioExiste añadidos

--- Juego/src/db/GestorBD.java
+++ Juego/src/db/GestorBD.java
@@ -62,12 +62,12 @@ public class GestorBD {
 	 * @param nombreUsuario
 	 * @return devuelve su codigo de partida. Si no ha encontrado el usuario devuelve 0
 	 */
-	public int obtenerCodigoPartida(String nombreUsuario) {
+	public int obtenerCodigoPartida(String nombreUsuario, String contraseña) {
 		int codigo = 0;
 		try(PreparedStatement stmt = conn.prepareStatement("SELECT codPartida FROM Usuario"
-				+ "WHERE nombre = ?")) {
+				+ "WHERE nombre = ? and password = ?")) {
 			stmt.setString(1, nombreUsuario);
-			
+			stmt.setString(2, contraseña);
 			ResultSet rs = stmt.executeQuery();
 			while (rs.next()) {
 				codigo = rs.getInt("codPartida");
@@ -81,11 +81,11 @@ public class GestorBD {
 	}
 	
 	/**
-	 * Metodo que carga todos los Usuarios con su nombre(clave) y contraseña(valor) en un mapa 
+	 * Metodo que carga todos los Usuarios con su codigo de partida(clave) y el usuario(valor) en un mapa 
 	 * @return Devuelve un mapa con todos los Usuarios
 	 */
-	public Map<String, String> cargarUsuarios() {
-		Map<String, String> mapaUsuarios = new HashMap<String, String>();
+	public Map<Integer, Usuario> cargarUsuarios() {
+		Map<Integer, Usuario> mapaUsuarios = new HashMap<Integer, Usuario>();
 		//TODO seguir programando metodo 
 		
 		
@@ -107,9 +107,9 @@ public class GestorBD {
 	 * @param nombreUsuario, nombre del usuario del que se quiere cargar la partida
 	 * @return devuelve una lista de Mejoras
 	 */
-	public List<Mejora> cargarPartida(String nombreUsuario) {
+	public List<Mejora> cargarPartida(String nombreUsuario, String contraseña) {
 		List<Mejora> listaMejoras = new ArrayList<Mejora>();
-		int codPartida = obtenerCodigoPartida(nombreUsuario);
+		int codPartida = obtenerCodigoPartida(nombreUsuario, contraseña);
 		//TODO seguir programando metodo
 		try (PreparedStatement cargaPartida = conn.prepareStatement("SELECT * FROM Mejora WHERE codPartida = ?")) {
 			cargaPartida.setInt(1, codPartida);
@@ -137,14 +137,44 @@ public class GestorBD {
 	}
 	
 	/**
+	 * Metodo que carga los datos de un usuario desde la BD
+	 * @param nombreUsuario
+	 * @param contraseña
+	 * @return devuelve un Usuario o null en caso de que no se encuentre
+	 */
+	public Usuario cargarUsuario(String nombreUsuario, String contraseña) {
+		
+		try(PreparedStatement cargaUsuario = conn.prepareStatement("SELECT * FROM Usuario WHERE nombre = ? and password = ?")) {
+			cargaUsuario.setString(1, nombreUsuario);
+			cargaUsuario.setString(2, contraseña);
+			
+			ResultSet rs = cargaUsuario.executeQuery();
+			 while (rs.next()) {
+				 int codPartida = rs.getInt("codPartida");
+				 float puntos = rs.getFloat("puntos");
+				 int creditos = rs.getInt("creditos");
+				 
+				 return new Usuario(nombreUsuario, contraseña, codPartida, puntos, creditos);
+			 }
+			
+			
+			
+		} catch (SQLException e) {
+			System.out.println("Error al cargar usuario desde la BD");
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
 	 * Metodo que guarda la partida del usuario en la BD a partir de su nombre
 	 * Hacer con UPDATE Mejora 
 	 * set precio = ? ...
 	 * where codPartida = ? and nombre = mejora.getNombre();
 	 * @param nombreUsuario
 	 */
-	public void guardarPartida(String nombreUsuario, ArrayList<Mejora> listaMejoras) {
-		int codPartida = obtenerCodigoPartida(nombreUsuario);
+	public void guardarPartida(String nombreUsuario, String contraseña, ArrayList<Mejora> listaMejoras) {
+		int codPartida = obtenerCodigoPartida(nombreUsuario, contraseña);
 		if (codPartida != 0) {
 			try (PreparedStatement guardarMejoras = conn.prepareStatement("UPDATE Mejora set"
 					+ "numero = ? ," //1
@@ -152,7 +182,7 @@ public class GestorBD {
 					+ "ganancia = ? ," //3
 					+ "multiplicador = ? ," //4
 					+ "bonificador = ? ," //5
-					+ "WHERE codPartida = ? and nombre = ?")) { // 6 y 7
+					+ "WHERE codPartida = ? and nombre = ?")) { // 6 y 7 (el nombre se refiere al nombre de la Mejora)
 				
 			for (Mejora mejora : listaMejoras) {
 				guardarMejoras.setInt(1, mejora.getNumero());
@@ -171,11 +201,63 @@ public class GestorBD {
 				e.printStackTrace();
 				
 			}
-
-			
-			
+		
 		} else {
 			System.out.println("Usuario o partida no encontrados");
+		}
+	}
+	
+	/**
+	 * Metodo que comprueba si el el nombre del usuario con una contraseña existe ya en la bd
+	 * @param nombreUsuario
+	 * @param contraseña
+	 * @return devuelve true si existe y false si no existe
+	 */
+	public boolean comprobarSiUsuarioExiste(String nombreUsuario, String contraseña) {
+	//Primero comprobar que no existe ya el usuario con la misma contraseña
+			try (PreparedStatement comprobarSiExiste = conn.prepareStatement("SELECT COUNT(*) FROM Usuario WHERE nombre = ? and password = ?")) {
+				comprobarSiExiste.setString(1, nombreUsuario);
+				comprobarSiExiste.setString(2, contraseña);
+				ResultSet rs = comprobarSiExiste.executeQuery();
+				
+				if (rs.next() && rs.getInt(1) > 0) {
+	                System.out.println("El usuario existe en la base de datos.");
+	                return true;
+	            } else {
+	                System.out.println("El usuario NO existe en la base de datos.");
+	                return false;
+	            }
+				
+			} catch (SQLException e) {
+				System.out.println("Error al comprobar si el usuario existe en la BD");
+				e.printStackTrace();
+			}
+			return false;
+	}
+	
+	/**
+	 * Metodo que inserta un nuevo usuario en la BD, utilizando su nombre y contraseña
+	 * @param nombreUsuario
+	 * @param contraseña
+	 */
+	public void insertarNuevoUsuario(String nombreUsuario, String contraseña) {
+		if (!comprobarSiUsuarioExiste(nombreUsuario, contraseña)) {
+			
+			try (PreparedStatement insertarUsuario = conn.prepareStatement("INSERT INTO Usuario "
+					+ "(nombre, password, ultimaConexion, puntos, creditos) VALUES (?, ?, ?, ?, ?)")) {
+				
+				insertarUsuario.setString(1, nombreUsuario);
+				insertarUsuario.setString(2, contraseña);
+				insertarUsuario.setLong(3, System.currentTimeMillis());
+				insertarUsuario.setFloat(4, 0);
+				insertarUsuario.setInt(5, 0);
+				
+			} catch (SQLException e) {
+				System.out.println("Error al insertar nuevo Usuario");
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("Este usuario ya existe en la BD");
 		}
 	}
 	
